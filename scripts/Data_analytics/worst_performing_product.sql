8d1ffdd20bbe2fcf71c854eac824978a8c7fbaa4
Md. Ahsan Ul Haque <ahsan.adnan1221@gmail.com>
2025-09-05 11:06:17 +0600

    Create worst_performing_product.sql

---
+++ scripts/Data_analytics/worst_performing_product.sql
@@ -0,0 +1,10 @@
+-- What  are the 5 worst performing products in terms of sales
+
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC
